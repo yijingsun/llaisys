04d6f289ab6734f43fb086f91646ab2becba36b1
Joy <869780236@qq.com>
2026-07-27 21:53:12 +0800

    feat: finish linear cpu operator

---
+++ src/ops/linear/cpu/linear_cpu.cpp
@@ -0,0 +1,54 @@
+#include "linear_cpu.hpp"
+
+#include "../../../utils.hpp"
+
+#include <cmath>
+
+template <typename T>
+void linear_(T *out, const T *in, const T *weight, const T *bias, size_t batch_size, size_t in_features, size_t out_features) {
+    for (size_t i = 0; i < batch_size; i++) {
+        for (size_t j = 0; j < out_features; j++) {
+            float sum = 0.0f;
+            for (size_t k = 0; k < in_features; k++) {
+                if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                    sum += llaisys::utils::cast<float>(in[i * in_features + k]) * llaisys::utils::cast<float>(weight[j * in_features + k]);
+                } else {
+                    sum += in[i * in_features + k] * weight[j * in_features + k];
+                }
+            }
+
+            float bias_val = 0.0f;
+            if (bias != nullptr) {
+                if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                    bias_val = llaisys::utils::cast<float>(bias[j]);
+                } else {
+                    bias_val = bias[j];
+                }
+            }
+
+            sum = sum + bias_val;
+            if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                out[i * out_features + j] = llaisys::utils::cast<T>(sum);
+            } else {
+                out[i * out_features + j] = sum;
+            }
+        }
+    }
+}
+
+namespace llaisys::ops::cpu {
+void linear(std::byte *out, const std::byte *in, const std::byte *weight, const std::byte *bias, llaisysDataType_t type, size_t batch_size, size_t in_features, size_t out_features) {    
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return linear_(reinterpret_cast<float *>(out), reinterpret_cast<const float *>(in), reinterpret_cast<const float *>(weight), reinterpret_cast<const float *>(bias), batch_size, in_features, out_features);
+    case LLAISYS_DTYPE_BF16:
+        return linear_(reinterpret_cast<llaisys::bf16_t *>(out), reinterpret_cast<const llaisys::bf16_t *>(in),
+                       reinterpret_cast<const llaisys::bf16_t *>(weight), reinterpret_cast<const llaisys::bf16_t *>(bias), batch_size, in_features, out_features);
+    case LLAISYS_DTYPE_F16:
+        return linear_(reinterpret_cast<llaisys::fp16_t *>(out), reinterpret_cast<const llaisys::fp16_t *>(in),
+                       reinterpret_cast<const llaisys::fp16_t *>(weight), reinterpret_cast<const llaisys::fp16_t *>(bias), batch_size, in_features, out_features);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
+}
+} // namespace llaisys::ops::cpu
--- src/ops/linear/op.cpp
+++ src/ops/linear/op.cpp
@@ -1,7 +1,48 @@
 #include "op.hpp"
 
+#include "../../core/llaisys_core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/linear_cpu.hpp"
+
 namespace llaisys::ops {
 void linear(tensor_t out, tensor_t in, tensor_t weight, tensor_t bias) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, in, weight, bias);
+    // Only support contiguous inputs for now.
+    ASSERT(out->isContiguous() && in->isContiguous() && weight->isContiguous() && bias->isContiguous(), "Linear: all tensors must be contiguous.");
+    CHECK_SAME_DTYPE(out->dtype(), in->dtype(), weight->dtype(), bias->dtype());
+    
+    // in [batch, in_features], weight [out_features, in_features], bias [out_features], out [batch, out_features]
+    // out = in * weight^T + bias
+    CHECK_ARGUMENT(in->shape()[0] == out->shape()[0], "linear: input tensor's first dimension must match output tensor's first dimension.");
+    CHECK_ARGUMENT(in->shape()[1] == weight->shape()[1], "linear: input tensor's second dimension must match weight tensor's second dimension.");
+    CHECK_ARGUMENT(out->shape()[1] == weight->shape()[0], "linear: output tensor's second dimension must match weight tensor's first dimension.");
+    
+    std::byte *bias_data = nullptr;
+    if (bias != nullptr) {
+        CHECK_ARGUMENT(bias->shape()[0] == weight->shape()[0], "linear: bias tensor's first dimension must match weight tensor's first dimension.");
+        bias_data = bias->data();
+    }
+
+    // always support cpu calculation
+    if (out->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::linear(out->data(), in->data(), weight->data(), bias_data, out->dtype(),
+        in->shape()[0], in->shape()[1], out->shape()[1]);
+    }
+
+    llaisys::core::context().setDevice(out->deviceType(), out->deviceId());
+
+    switch (out->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::linear(out->data(), in->data(), weight->data(), bias_data, out->dtype(),
+        in->shape()[0], in->shape()[1], out->shape()[1]);
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
