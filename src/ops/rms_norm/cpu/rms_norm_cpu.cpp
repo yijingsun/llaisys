239ab84a05ee2d49366046f354bb19fc410fb903
Joy <869780236@qq.com>
2026-07-28 00:08:08 +0800

    feat: finish rms_norm cpu operator

---
+++ src/ops/rms_norm/cpu/rms_norm_cpu.cpp
@@ -0,0 +1,54 @@
+#include "rms_norm_cpu.hpp"
+
+#include "../../../utils.hpp"
+
+#include <cmath>
+
+template <typename T>
+void rms_norm_(T *out, const T *in, const T *weight, float eps, size_t batch_size, size_t in_features) {
+    for (size_t i = 0; i < batch_size; i++) {
+        float sum_square = 0.0f;
+        for (size_t j = 0; j < in_features; j++) {
+            float val;
+            if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                val = llaisys::utils::cast<float>(in[i * in_features + j]);
+            } else {
+                val = in[i * in_features + j];
+            }
+            sum_square += val * val;
+        }
+        float mean_square = sum_square / static_cast<int64_t>(in_features) + eps;
+        float rms = std::sqrt(mean_square);
+
+        for (size_t j = 0; j < in_features; j++) {
+            float result = 0.0f;
+            
+            if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                result = llaisys::utils::cast<float>(in[i * in_features + j]) * llaisys::utils::cast<float>(weight[j]) / rms;
+            } else {
+                result = in[i * in_features + j] * weight[j] / rms;
+            }
+
+            if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                out[i * in_features + j] = llaisys::utils::cast<T>(result);
+            } else {
+                out[i * in_features + j] = result;
+            }
+        }
+    }
+}
+
+namespace llaisys::ops::cpu {
+void rms_norm(std::byte *out, const std::byte *in, const std::byte *weight, float eps, llaisysDataType_t type, size_t batch_size, size_t in_features) {
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return rms_norm_(reinterpret_cast<float *>(out), reinterpret_cast<const float *>(in), reinterpret_cast<const float *>(weight), eps, batch_size, in_features);
+    case LLAISYS_DTYPE_BF16:
+        return rms_norm_(reinterpret_cast<llaisys::bf16_t *>(out), reinterpret_cast<const llaisys::bf16_t *>(in), reinterpret_cast<const llaisys::bf16_t *>(weight), eps, batch_size, in_features);
+    case LLAISYS_DTYPE_F16:
+        return rms_norm_(reinterpret_cast<llaisys::fp16_t *>(out), reinterpret_cast<const llaisys::fp16_t *>(in), reinterpret_cast<const llaisys::fp16_t *>(weight), eps, batch_size, in_features);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
+}   
+} // namespace llaisys::ops::cpu
---
+++ src/ops/rms_norm/cpu/rms_norm_cpu.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "llaisys.h"
+
+#include <cstddef>
+
+namespace llaisys::ops::cpu {
+void rms_norm(std::byte *out, const std::byte *in, const std::byte *weight, float eps, llaisysDataType_t type, size_t batch_size, size_t in_features);
+}
