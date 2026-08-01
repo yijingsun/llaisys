16de7bb1623de7f1009b412d9eac151f8739b08d
Joy <869780236@qq.com>
2026-08-01 21:34:35 +0800

    feat: finish swiglu cpu operator

---
+++ src/ops/swiglu/cpu/swiglu_cpu.cpp
@@ -0,0 +1,35 @@
+#include "swiglu_cpu.hpp"
+
+#include "../../../utils.hpp"
+
+#include <cmath>
+
+template <typename T>
+void swiglu_(T *out, const T *gate, const T *up, size_t numel) {
+    // SwiGLU : out = up * sigmoid(gate) = up * (gate / (1 + exp(-gate)))
+    for (size_t i = 0; i < numel; i++) {
+            if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                float up_val = llaisys::utils::cast<float>(up[i]);
+                float gate_val = llaisys::utils::cast<float>(gate[i]);
+                float sigmoid_val = 1.0f / (1.0f + std::exp(-gate_val));
+                out[i] = llaisys::utils::cast<T>(up_val * gate_val * sigmoid_val);
+            } else {
+                out[i] = up[i] * (gate[i] / (1.0f + std::exp(-gate[i])));
+            }
+    }
+}
+
+namespace llaisys::ops::cpu {
+void swiglu(std::byte* out, const std::byte* gate, const std::byte* up, llaisysDataType_t type, size_t numel) {
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return swiglu_(reinterpret_cast<float *>(out), reinterpret_cast<const float *>(gate), reinterpret_cast<const float *>(up), numel);
+    case LLAISYS_DTYPE_BF16:
+        return swiglu_(reinterpret_cast<llaisys::bf16_t *>(out), reinterpret_cast<const llaisys::bf16_t *>(gate), reinterpret_cast<const llaisys::bf16_t *>(up), numel);
+    case LLAISYS_DTYPE_F16:
+        return swiglu_(reinterpret_cast<llaisys::fp16_t *>(out), reinterpret_cast<const llaisys::fp16_t *>(gate), reinterpret_cast<const llaisys::fp16_t *>(up), numel);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
+}
+} // namespace llaisys::ops::cpu
--- src/ops/swiglu/op.cpp
+++ src/ops/swiglu/op.cpp
@@ -1,7 +1,36 @@
 #include "op.hpp"
 
+#include "../../core/core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/swiglu_cpu.hpp"
+
 namespace llaisys::ops {
 void swiglu(tensor_t out, tensor_t gate, tensor_t up) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, gate, up);
+    // Only support contiguous inputs for now.
+    ASSERT(out->isContiguous() && gate->isContiguous() && up->isContiguous(), "SwiGLU: all tensors must be contiguous.");
+    CHECK_SAME_DTYPE(out->dtype(), gate->dtype(), up->dtype());
+
+    // out, gate, up [seqlen, intermediate_size] 
+    CHECK_ARGUMENT(gate->shape() == up->shape() && out->shape() == gate->shape(), "SwiGLU: gate and up tensors must have the same shape.");
+    // always support cpu calculation
+    if (out->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::swiglu(out->data(), gate->data(), up->data(), out->dtype(), up->numel());
+    }
+
+    llaisys::core::context().setDevice(out->deviceType(), out->deviceId());
+
+    switch (out->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::swiglu(out->data(), gate->data(), up->data(), out->dtype(), up->numel());
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
