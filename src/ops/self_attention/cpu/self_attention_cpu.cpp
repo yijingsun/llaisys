ae0e4bea9a78f3524c1d15471fca9f01cc2d1369
Joy <869780236@qq.com>
2026-08-01 18:29:47 +0800

    feat: finish self_attention cpu operator

---
+++ src/ops/self_attention/cpu/self_attention_cpu.cpp
@@ -0,0 +1,73 @@
+#include "self_attention_cpu.hpp"
+
+#include "../../../utils.hpp"
+
+#include <cmath>
+
+template <typename T>
+void self_attention_(T *attn_val, const T *q, const T *k, const T *v, float scale, size_t seqlen, size_t nhead, size_t d, size_t total_len, size_t nkvhead, size_t dv) {
+    // mask[i][j] = -inf if j > total_len - seqlen + i else 0
+    float mask[seqlen][total_len];
+    for (size_t i = 0; i < seqlen; i++) {
+        for (size_t j = 0; j < total_len; j++) {
+            if (j > total_len - seqlen + i) {
+                mask[i][j] = -INFINITY;
+            } else {
+                mask[i][j] = 0.0f;
+            }
+        }
+    }
+    for (size_t j = 0; j < nhead; j++) {
+        int head_idx = j * nkvhead / nhead;
+        for (size_t i = 0; i < seqlen; i++) {
+            float sum_exp_scores = 0.0f;
+            float masked_exp_scores[total_len];
+
+            for (size_t k_idx = 0; k_idx < total_len; k_idx++) {
+                float dot_product = 0.0f;
+                for (size_t n = 0; n < d; n++) {
+                    if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                        dot_product += llaisys::utils::cast<float>(q[i * nhead * d + j * d + n]) * llaisys::utils::cast<float>(k[k_idx * nkvhead * d + head_idx * d + n]);
+                    } else {
+                        dot_product += q[i * nhead * d + j * d + n] * k[k_idx * nkvhead * d + head_idx * d + n];
+                    }
+                }
+                masked_exp_scores[k_idx] = std::exp(dot_product * scale + mask[i][k_idx]); // apply mask and compute scaled exp scores
+                sum_exp_scores += masked_exp_scores[k_idx];
+            }
+
+            for (size_t nv = 0; nv < dv; nv++) {
+                float attn_value = 0.0f;
+                for (size_t v_idx = 0; v_idx < total_len; v_idx++) {
+                    float softmax_score = masked_exp_scores[v_idx] / sum_exp_scores; // softmax = exp_score / sum_exp_scores
+                    if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                        attn_value += softmax_score * llaisys::utils::cast<float>(v[v_idx * nkvhead * dv + head_idx * dv + nv]);
+                    } else {
+                        attn_value += softmax_score * v[v_idx * nkvhead * dv + head_idx * dv + nv];
+                    }
+                }
+                if constexpr (std::is_same_v<T, llaisys::bf16_t> || std::is_same_v<T, llaisys::fp16_t>) {
+                    attn_val[i * nhead * dv + j * dv + nv] = llaisys::utils::cast<T>(attn_value);
+                } else {
+                    attn_val[i * nhead * dv + j * dv + nv] = attn_value;
+                }
+            }
+        }
+    }
+}
+
+namespace llaisys::ops::cpu {
+void self_attention(std::byte* attn_val, const std::byte* q, const std::byte* k, const std::byte* v, float scale, llaisysDataType_t type,
+                    size_t seqlen, size_t nhead, size_t d, size_t total_len, size_t nkvhead, size_t dv) {
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return self_attention_(reinterpret_cast<float *>(attn_val), reinterpret_cast<const float *>(q), reinterpret_cast<const float *>(k), reinterpret_cast<const float *>(v), scale, seqlen, nhead, d, total_len, nkvhead, dv);
+    case LLAISYS_DTYPE_BF16:
+        return self_attention_(reinterpret_cast<llaisys::bf16_t *>(attn_val), reinterpret_cast<const llaisys::bf16_t *>(q), reinterpret_cast<const llaisys::bf16_t *>(k), reinterpret_cast<const llaisys::bf16_t *>(v), scale, seqlen, nhead, d, total_len, nkvhead, dv);
+    case LLAISYS_DTYPE_F16:
+        return self_attention_(reinterpret_cast<llaisys::fp16_t *>(attn_val), reinterpret_cast<const llaisys::fp16_t *>(q), reinterpret_cast<const llaisys::fp16_t *>(k), reinterpret_cast<const llaisys::fp16_t *>(v), scale, seqlen, nhead, d, total_len, nkvhead, dv);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
+}
+} // namespace llaisys::ops::cpu
--- src/ops/self_attention/op.cpp
+++ src/ops/self_attention/op.cpp
@@ -1,7 +1,46 @@
 #include "op.hpp"
 
+#include "../../core/core.hpp"
+#include "../../utils.hpp"
+
+#include "cpu/self_attention_cpu.hpp"
+
 namespace llaisys::ops {
 void self_attention(tensor_t attn_val, tensor_t q, tensor_t k, tensor_t v, float scale) {
-    TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(attn_val, q, k, v);
+    // Only support contiguous inputs for now.
+    ASSERT(attn_val->isContiguous() && q->isContiguous() && k->isContiguous() && v->isContiguous(), "Self-Attention: all tensors must be contiguous.");
+    CHECK_SAME_DTYPE(attn_val->dtype(), q->dtype(), k->dtype(), v->dtype());
+    
+    // atten_val [seqlen, nhead, dv]
+    // q [seqlen, nhead, d], k  [total_len, nkvhead, d], v [total_len, nkvhead, dv]
+    // total_len = seqlen + past_len, nkvhead = nhead * nkv_ratio
+    CHECK_ARGUMENT(k->shape() == v->shape(), "Self -Attention: key and value tensors must have the same shape.");
+    CHECK_ARGUMENT(attn_val->shape()[0] == q->shape()[0], "Self-Attention: attention value and query tensors must have the same sequence length.");
+    CHECK_ARGUMENT(attn_val->shape()[1] == q->shape()[1], "Self-Attention: attention value and query tensors must have the same number of heads.");
+    CHECK_ARGUMENT(attn_val->shape()[2] == v->shape()[2], "Self-Attention: attention value and value tensors must have the same dimension.");
+    // less kvhead less attention compute, less k/v cache
+    CHECK_ARGUMENT(q->shape()[1] % k->shape()[1] == 0, "Self-Attention: number of heads in query must be divisible by number of heads in key/value.");
+    
+    // always support cpu calculation
+    if (attn_val->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::self_attention(attn_val->data(), q->data(), k->data(), v->data(), scale, attn_val->dtype(),
+        q->shape()[0], q->shape()[1], q->shape()[2], k->shape()[0], k->shape()[1], k->shape()[2]);
+    }
+
+    llaisys::core::context().setDevice(attn_val->deviceType(), attn_val->deviceId());
+
+    switch (attn_val->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::self_attention(attn_val->data(), q->data(), k->data(), v->data(), scale, attn_val->dtype(),
+        q->shape()[0], q->shape()[1], q->shape()[2], k->shape()[0], k->shape()[1], k->shape()[2]);
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
